5af453c685088c8bc63105b75e3dbce435a0f8a7
Dave <38231499+det-builder@users.noreply.github.com>
2018-04-19 09:01:33 -0400

    Cleanup the script.

--- BackupDrive/Sync.sh
+++ BackupDrive/Sync.sh
@@ -1,9 +1,16 @@
-# Steps to run this script:
-# 1) Open Terminal and run the command "sudo su - root" - pwd is the g password
-# 2) Run the command "cd /Volumes/BackupDrive"
-# 3) Run the command "./Sync.sh"
-
-echo Backing up D Drive
+#######################################################################################
+# Sync.sh                                                                             #
+#                                                                                     #
+# This script is used to backup and sync several folders on my Mac to my external     #
+# hard drive that is kept off-site.                                                   #
+#                                                                                     #
+# Usage Instructions                                                                  #
+#                                                                                     #
+# 1) Open Terminal and run the command "sudo su - root".  The pwd is my g password.   #
+# 2) Run the command "cd /Volumes/BackupDrive"                                        #
+# 3) Run the command "sh Sync.sh"                                                     #
+#                                                                                     #
+#######################################################################################
 
 # Sync folders on the D Drive
 rsync -a --delete /Volumes/D\ Drive/My_Apps /Volumes/BackupDrive/D_Drive  
@@ -13,8 +20,6 @@ rsync -a --delete /Volumes/D\ Drive/My_Movies /Volumes/BackupDrive/D_Drive
 rsync -a --delete /Volumes/D\ Drive/My_Music /Volumes/BackupDrive/D_Drive 
 rsync -a --delete /Volumes/D\ Drive/My_Pictures /Volumes/BackupDrive/D_Drive 
 rsync -a --delete /Volumes/D\ Drive/My_VMs /Volumes/BackupDrive/D_Drive 
-
-echo Backing up C Drive
 
 # Sync folders on the C Drive
 rsync -a --delete /Users /Volumes/BackupDrive/C_Drive
