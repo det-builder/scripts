ee49cc0d59761402bbb4ff8f4117ff232c0d8ad3
Dave <38231499+det-builder@users.noreply.github.com>
2018-05-14 21:44:09 -0400

    Update Remove_Builtin_Apps.ps1

--- Debloat/Remove_Builtin_Apps.ps1
+++ Debloat/Remove_Builtin_Apps.ps1
@@ -1,7 +1,7 @@
 # This is my script to remove the *useless* built-in Windows 10 apps from a new install.
   
 # Alarms and clock
-Get-AppxPackage *windowsalarms* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.WindowsAlarms" | Remove-AppxPackage
   
 # Bing stuff (finance, news, sports & weather)
 Get-AppxPackage -Name "Microsoft.BingFinance" | Remove-AppxPackage
@@ -10,13 +10,13 @@ Get-AppxPackage -Name "Microsoft.BingSports" | Remove-AppxPackage
 Get-AppxPackage -Name "Microsoft.BingWeather" | Remove-AppxPackage
   
 # Calendar
-Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage -Name "microsoft.windowscommunicationsapps" | Remove-AppxPackage
   
 # Calculator
-# Get-AppxPackage *calculator* | Remove-AppxPackage
+# Get-AppxPackage -name "Microsoft.WindowsCalculator" | Remove-AppxPackage
   
 # Camera
-Get-AppxPackage *windowscamera* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.WindowsCamera" | Remove-AppxPackage
   
 # DisneyMagicKingdoms
 Get-AppxPackage -Name "MA278AB0D.DisneyMagicKingdoms" | Remove-AppxPackage
@@ -28,66 +28,56 @@ Get-AppxPackage -Name "DolbyLaboratories.DolbyAccess" | Remove-AppxPackage
 Get-AppxPackage *Facebook* | Remove-AppxPackage
 
 # Feedback Hub
-Get-AppxPackage *WindowsFeedbackHub* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.WindowsFeedbackHub" | Remove-AppxPackage
   
 # Get Help
-Get-AppxPackage *Microsoft.GetHelp* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.GetHelp" | Remove-AppxPackage
   
 # Get Office
-Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.MicrosoftOfficeHub" | Remove-AppxPackage
   
 # Get Skype
-Get-AppxPackage *skypeapp* | Remove-AppxPackage
+# Get-AppxPackage -Name "Microsoft.SkypeApp" | Remove-AppxPackage
   
 # Groove Music
-Get-AppxPackage *zunemusic* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.ZuneMusic" | Remove-AppxPackage
   
 # Hidden City Game
 Get-AppxPackage -Name "828B5831.HiddenCityMysteryofShadows" | Remove-AppxPackage
   
 # Maps
-Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.WindowsMaps" | Remove-AppxPackage
 
 # March of Empires
 Get-AppxPackage -Name "A278AB0D.MarchofEmpires" | Remove-AppxPackage
   
 # Messaging
-Get-appxpackage *messaging* | remove-appxpackage
+Get-appxpackage -Name "Microsoft.Messaging" | remove-appxpackage
 
 # Microsoft crap
 Get-AppxPackage -Name "Microsoft.MicrosoftPowerBIForWindows" | Remove-AppxPackage
 Get-AppxPackage -Name "Microsoft.NetworkSpeedTest" | Remove-AppxPackage
-Get-AppxPackage *ConnectivityStore* | Remove-AppxPackage
 
 # Minecraft
 Get-AppxPackage -Name "Microsoft.MinecraftUWP" | Remove-AppxPackage
   
-# Mobile Plans
-Get-AppxPackage *Microsoft.OneConnect* | Remove-AppxPackage
-
-# Money
-Get-AppxPackage *bingfinance* | Remove-AppxPackage
-  
 # Movies & TV
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
- 
-# My Office
-Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage
+# Get-AppxPackage *zunevideo* | Remove-AppxPackage
 
 # Onenote
-Get-AppxPackage *onenote* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.Office.OneNote" | Remove-AppxPackage
   
 # Paid Wifi & Cellular
-Get-AppxPackage *OneConnect* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.OneConnect" | Remove-AppxPackage
   
 # People
-Get-AppxPackage *people* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.People" | Remove-AppxPackage
   
 # Phone & Phone Companion
 get-appxpackage *phone* | remove-appxpackage
   
 # Paint 3D
-# Get-AppxPackage *MSPaint* | Remove-AppxPackage
+# Get-AppxPackage -Name "Microsoft.MSPaint" | Remove-AppxPackage
   
 # Photos
 Get-AppxPackage *photos* | Remove-AppxPackage
@@ -99,16 +89,16 @@ Get-AppxPackage *reader* | remove-appxpackage
 Get-AppxPackage *WindowsScan* | Remove-AppxPackage
   
 # Solitaire
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.MicrosoftSolitaireCollection" | Remove-AppxPackage
   
 # Sticky Notes
-Get-AppxPackage *Stickynotes* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.MicrosoftStickyNotes" | Remove-AppxPackage
   
 # Sway
 Get-AppxPackage *Office.Sway* | Remove-AppxPackage
   
 # Tips
-Get-AppxPackage *Getstarted* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.Getstarted" | Remove-AppxPackage
   
 # Useless games & crap
 Get-AppxPackage -Name "king.com.CandyCrushSaga" | Remove-AppxPackage
@@ -119,13 +109,21 @@ Get-AppxPackage -Name "king.com.*" | Remove-AppxPackage
 Get-AppxPackage *soundrecorder* | Remove-AppxPackage
   
 # View 3D
-Get-AppxPackage *Microsoft3DViewer* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.Microsoft3DViewer" | Remove-AppxPackage
   
 # XBox
-Get-AppxPackage *xbox* | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.Xbox.TCUI" | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.XboxApp" | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.XboxGameOverlay" | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.XboxGamingOverlay" | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.XboxIdentityProvider" | Remove-AppxPackage
+Get-AppxPackage -Name "Microsoft.XboxSpeechToTextOverlay" | Remove-AppxPackage
   
 # To reinstall ALL Windows default apps, run this command:
 Get-AppXPackage | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
   
 # To get a listing of installed packages, run this command:
 Get-AppxPackage | select name, packagefullname | d:\packages.txt
+
+# To get a listing of the default apps windows installs for a new user:
+Get-ProvisionedAppXPackage -Online|Select DisplayName
