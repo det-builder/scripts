40ce7e3bd5890acc4b45a0e60fc58976c6270039
Dave <38231499+det-builder@users.noreply.github.com>
2018-07-01 18:31:35 -0400

    Create Remove_MixedReality.bat

---
+++ Debloat/Remove_MixedReality.bat
@@ -0,0 +1,5 @@
+REM NOTE!  This script needs to be ran with an elevated command prompt.
+REM
+
+REM Rename this directory to something else.
+REN "C:\Windows\SystemApps\Microsoft.Windows.HolographicFirstRun_cw5n1h2txyewy" "C:\Windows\SystemApps\OLD_Microsoft.Windows.HolographicFirstRun_cw5n1h2txyewy" 
