56a1305715375a3cd697d20ecba494076f4bf3da
Dave <38231499+det-builder@users.noreply.github.com>
2018-08-22 17:05:43 -0400

    Update Relocate_iTunes_Backups.bat

--- Debloat/Relocate_iTunes_Backups.bat
+++ Debloat/Relocate_iTunes_Backups.bat
@@ -4,13 +4,13 @@ REM Note, be sure to put in the correct "<user>" value in the command below.
 REM
 
 REM NOTE! This directory is optional to delete.  If there are backups in this directory
-REM it is safe to not execute this command.
+REM you wish to keep then don't execute this command.
 rmdir /q /s D:\My_Apps\iTunes_Backups\Backup
 
 REM Delete the directory that we're moving.  NOTE, with the move to the MS Store, the location
 REM for iTunes backup was changed.  I left the previous version in here.
 REM del "C:\Users\<user>\AppData\Roaming\Apple Computer\MobileSync\Backup"
-del "C:\Users\<user>\Apple\MobileSync\Backup"
+rmdir /q /s "c:\users\<user>\apple\mobilesync\backup"
 
 REM Execute the mklink command to create a link between the two directories.  NOTE, with the move to the MS Store, the location
 REM for iTunes backup was changed.  I left the previous version in here.
