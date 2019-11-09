60bf7ff1561d54d30e69718cfc08d70060495aac
Dave <38231499+det-builder@users.noreply.github.com>
2019-11-08 19:25:08 -0500

    Update Relocate_iTunes_Backups.bat

--- Debloat/Relocate_iTunes_Backups.bat
+++ Debloat/Relocate_iTunes_Backups.bat
@@ -9,12 +9,9 @@ rmdir /q /s D:\My_Apps\iTunes_Backups\Backup
 
 REM Delete the directory that we're moving.  NOTE, with the move to the MS Store, the location
 REM for iTunes backup was changed.  I left the previous version in here.
-REM del "C:\Users\<user>\AppData\Roaming\Apple Computer\MobileSync\Backup"
 rmdir /q /s "c:\users\<user>\apple\mobilesync\backup"
 
 REM Execute the mklink command to create a link between the two directories.  NOTE, with the move to the MS Store, the location
 REM for iTunes backup was changed.  I left the previous version in here.
-REM mklink /j "C:\Users\<user>\AppData\Roaming\Apple Computer\MobileSync\Backup" "D:\My_Apps\iTunes_Backups\Backup"
-mklink /j "C:\Users\<user>\Apple\MobileSync\Backup" "D:\My_Apps\iTunes_Backups\Backup"
+mklink /j "C:\Users\<user>\Apple\MobileSync\Backup" "d:\My_Backups\iTunes_Backups"
 
-REM At this point, need to ensure the iTunes device backups exist inside of the "D:\My_Apps\iTunes_Backups\Backup" directory.
