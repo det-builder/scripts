75d20bf5d624d73364c52a53632bff58fb717994
Dave <38231499+det-builder@users.noreply.github.com>
2019-11-16 20:58:36 -0500

    Updates

--- Debloat/Relocate_Spotify_Cache.bat
+++ Debloat/Relocate_Spotify_Cache.bat
@@ -1,12 +1,12 @@
 REM ******************************************************************
-REM * Relocate_iTunes_Backups.bat                                    *
+REM * Relocate_Spotify_Cache.bat                                     *
 REM *                                                                *
-REM * This script will relocate the iTunes backup folder with within *
+REM * This script will relocate the Spotify cache folder with within *
 REM * the user profile folders to a more centralized location on the *
 REM * backup drive.                                                  *
 REM *                                                                *
 REM * Notes                                                          *
-REM * Be sure this script isn't ran until the iTunes app has been    *
+REM * Be sure this script isn't ran until the Spotify app has been   *
 REM * run at least once.  Doing so will ensure the basic directories *
 REM * are created and exist.                                         *
 REM *                                                                *
@@ -17,11 +17,11 @@ REM *                                                                *
 REM ******************************************************************
 
 REM Environment variables defined below.  Please change to current values.
-SET src_backup_dir="c:\users\%username%\apple\mobilesync\backup"
-set tgt_backup_dir="E:\My_Backups\iTunes_Backups"
+SET src_backup_dir="C:\Users\%username%\AppData\Local\Spotify\Storage"
+set tgt_backup_dir="E:\My_Spotify"
 
 REM Delete the directory that we're moving.  NOTE, this is the updated source
-REM directory used with the iTunes app moving to the Windows Store. 
+REM directory used with the Spotify app moving to the Windows Store. 
 if exist %src_backup_dir% rmdir /q /s %src_backup_dir%
 
 REM NOTE! This directory is optional to delete.  If there are backups in this directory
@@ -30,4 +30,3 @@ rmdir /q /s %tgt_backup_dir%
 
 REM Execute the mklink command to create a link between the two directories.  
 mklink /j %src_backup_dir% %tgt_backup_dir%
-
