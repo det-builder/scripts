4d30c757842d172e5d54a7017f5e93c419d3cdfb
Dave <38231499+det-builder@users.noreply.github.com>
2019-12-10 22:07:52 -0500

    Update exiftoolscripts.sh

---
+++ OSX_Stuff/exiftoolscripts.sh
@@ -0,0 +1,21 @@
+# This command will copy the EXIF Create date to the Create & Modify Date fields for ALL jpg's in directory.
+exiftool '-MDItemFSCreationDate<DateTimeOriginal' '-FileModifyDate<DateTimeOriginal' -overwrite_original_in_place -P -ext jpg -ext jpeg .
+
+# This command will show the properties and their values for a given image.
+exiftool -s image.jpg
+
+# This command will change the model of the camera that took the photo to something else while preserving last modify date.
+exiftool -P -Model='iPhone XS Max' image.jpg
+
+# This command creates a csv named "list.csv" in the current directory of the jpg's in the current diretory.
+exiftool -csv -FileModifyDate -FileAccessDate -FileCreateDate -MDItemFSCreationDate -ModifyDate -DateTimeOriginal -CreateDate *.jpg > list.csv
+
+# Misc Links
+https://exiftool.org/TagNames/
+https://exiftool.org/TagNames/MacOS.html
+https://exiftool.org/TagNames/EXIF.html
+
+# Note: These attributes can't be changed:
+# FileAccessDate                  : 2019:12:10 20:28:12-05:00
+# FileInodeChangeDate             : 2019:12:10 20:27:03-05:00
+
